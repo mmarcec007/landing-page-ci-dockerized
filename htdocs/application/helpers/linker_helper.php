599ff4234b10e4be5bb781d0cb368c56eec398b1
mark <mark.marcec@twofour.de>
2017-05-23 13:33:54 +0200

    Dockerized version

---
+++ htdocs/application/helpers/linker_helper.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('test_method'))
+{
+    function test_method()
+    {
+        return "ceca";
+    }
+
+    function ford_images()
+    {
+        return base_url('assets/images/ford/ford_image.jpg');
+    }
+
+    function ford_image($key)
+    {
+        return base_url('assets/images/ford/'.$key.'.jpg');
+    }
+
+    function to_view($view, $data)
+    {
+        return site_url($view.$data);
+    }   
+}
